331137d0de1bd9e5d79a483f8ab30d8f279da133
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-06-14 16:26:57 +0200

    add color vars to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ export default {
         outfit: ["Outfit", "sans-serif"],
         youngSerif: ["Young Serif", "serif"],
       },
+      colors: {
+        darkRaspberry: "hsla(332, 51%, 32%, 1)",
+        snow: "hsla(331, 100%, 98%, 1)",
+        wengeBrown: "hsla(30, 11%, 34%, 1)",
+      },
     },
   },
   plugins: [],
