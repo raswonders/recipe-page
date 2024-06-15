73c5d33069ae5bc55f8e52889631c3c2e3df557a
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-06-15 12:45:48 +0200

    fix: warning Heading levels should only increase by one

--- src/RecipePreparation.tsx
+++ src/RecipePreparation.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export function RecipeTimetable({ children }: { children: ReactNode }) {
   return (
     <section className="flex flex-col gap-4 p-6 bg-snow font-semibold rounded-xl">
-      <h3 className="text-darkRaspberry text-xl">Preparation time</h3>
+      <h2 className="text-darkRaspberry text-xl">Preparation time</h2>
       <ul className="space-y-2 ml-2">{children}</ul>
     </section>
   );
